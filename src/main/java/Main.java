9f3f38c4425097dfc27c70d69d7640ee9662ced9
pnowak86 <p_nowak86@poczta.fm>
2017-07-06 17:50:13 +0200

    third commit works fine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@
  */
 public class Main {
     public static void main(String[] args) {
+        CountLines cs = new CountLines();
+        System.out.println("Number of lines in file: " + cs.countLines("file.txt"));
 
     }
 }
--- src/test/java/CountLinesTest.java
+++ src/test/java/CountLinesTest.java
@@ -1,5 +1,8 @@
 /**
  * Created by RENT on 2017-07-06.
  */
-public class CountLinesTest {
+public class CountLinesTest{
+    public void should(){
+        
+    }
 }
