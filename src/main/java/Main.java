c63ec34c6d44b02fd4e77c468f58bae6a384e3a8
SzymonZiolo <szymz99@gmail.com>
2017-07-06 19:26:22 +0200

    Fixed the "wrong file" issue v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please select file");
         try {
+            System.out.println("Please select file");
             String filepath = sc.nextLine();
-
-        System.out.println("What would you like to count? Possible answers: words, lines, chars. Enter 'end' to end program");
-        String ans = sc.nextLine();
-        switch (ans) {
-            case "words":
-                CountWords cw = new CountWords();
-                System.out.println(cw.count(filepath));
-                break;
-            case "lines":
-                CountLines cl = new CountLines();
-                System.out.println(cl.countLines(filepath));
-                break;
-            case "chars":
-                CharsCounter cc = new CharsCounter();
-                System.out.println(cc.countLines(filepath));
-                break;
-        }
+            System.out.println("What would you like to count? Possible answers: words, lines, chars. Enter 'end' to end program");
+            String ans = sc.nextLine();
+            switch (ans) {
+                case "words":
+                    CountWords cw = new CountWords();
+                    System.out.println(cw.count(filepath));
+                    break;
+                case "lines":
+                    CountLines cl = new CountLines();
+                    System.out.println(cl.countLines(filepath));
+                    break;
+                case "chars":
+                    CharsCounter cc = new CharsCounter();
+                    System.out.println(cc.countLines(filepath));
+                    break;
+            }
         }catch(Exception e){
             System.err.println(e);
-            System.out.println("Wrong file name!");
         }
 
     }
