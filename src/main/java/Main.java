6e16ec99d425bb5a025019f75243ee8fd671e2b3
SzymonZiolo <szymz99@gmail.com>
2017-07-06 19:00:58 +0200

    Added menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 /**
  * Created by RENT on 2017-07-06.
  */
 public class Main {
     public static void main(String[] args) {
-        CountLines cs = new CountLines();
-        System.out.println("Number of lines in file: " + cs.countLines("file.txt"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please select file");
+        String filepath = sc.nextLine();
+        System.out.println("What would you like to count? Possible answers: words, lines, chars. Enter 'end' to end program");
+        String ans = sc.nextLine();
+        switch (ans) {
+            case "words":
+                CountWords cw = new CountWords();
+                System.out.println(cw.count(filepath));
+                break;
+            case "lines":
+                CountLines cl = new CountLines();
+                System.out.println(cl.countLines(filepath));
+                break;
+            case "chars":
+                CharsCounter cc = new CharsCounter();
+                System.out.println(cc.countLines(filepath));
+                break;
+        }
 
     }
 }
